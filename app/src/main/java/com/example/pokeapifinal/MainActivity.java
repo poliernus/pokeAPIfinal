f2a6844e1742030d895c98d5ed92df2ce848a81d
poliernus <polmeca@gmail.com>
2022-12-18 19:15:13 +0100

    main interfaz

--- app/src/main/java/com/example/pokeapifinal/MainActivity.java
+++ app/src/main/java/com/example/pokeapifinal/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.pokeapifinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = (Button) findViewById(R.id.buttonPokedex);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityLogin();
+            }
+        });
+    }
+    public void openActivityLogin(){
+        Intent intent = new Intent(this, activityLogin.class);
+        startActivity(intent);
     }
 }
